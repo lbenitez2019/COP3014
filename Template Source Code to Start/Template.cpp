c1cc723666ef37f4ac030c801442a2367719edb7
lbenitez2019 <lbenitez2019@fau.edu>
2022-09-19 20:46:28 -0400

    Added Template Source File

---
+++ Template Source Code to Start/Template.cpp
@@ -0,0 +1,19 @@
+/*
+COP3014: Programmming II
+Assignment: 
+Purpose: 
+Instructor: Sareh Taebi
+Author: Lucas Benitez
+*/
+#include <iostream>
+#include <string>       // Needed for type string
+#include <cmath>        // Needed for pow()
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
