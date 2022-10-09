90636b45b6dd41c79b3dabc5e1808a44fb842e4a
git-rafa <ralph.benitez@gmail.com>
2022-10-09 10:03:52 -0400

    - Updated list of requirements in main header. - Consolidated statements to print to screen. - Added variable initialization - Added type casting for constants of type SHORT being stored in a variable of type DOUBLE

--- A4/A4.cpp
+++ A4/A4.cpp
@@ -1,15 +1,31 @@
 // Course: COP3014
 // Date : OCT-7-2022
 // Assignment: 4
-// Purpose: REQ1 - Define a class named "cop3014" 
-//          REQ2 - cop3014 must have the following members
-//          REQ2 - cop3014 must have get/set functions for all variables
-//          REQ3 - cop3014 must have a function to calculates the weighted average numeric 
-//				   score for the entire course and sets the corresponding member variable
-//          REQ3 - cop3014 must have a function to calculate the final letter grade and 
-//				   sets the corresponding member variable
-//          REQ4 - cop3014 must have a function to display: z-number, and all grades to the screen.
-//          REQ4 - Test by generating multiple objects, and printing the student object’s record from COP3014.
+// Purpose:
+// Requirements: 
+//	REQ1  - Define a class named "cop3014" 
+//  REQ2  - cop3014 must have the following member VARIABLES:
+//			Student Z-number  
+//		 	Quiz1
+//			Quiz2
+//			Midterm Exam
+//			Final Exam
+//			Final Grade Avg %
+//			Final Letter Grade
+//	REQ3  - Quizzes have max score of 10
+//  REQ4  - Midterm and Final have max score of 100
+//  REQ5  - Final grade weights: Quizzes 10% each, Midterm 30%, Final 50%
+//  REQ6  - cop3014 must have a VOID FUNCTION to: 
+//				- calculate weighted numeric average score for the entire course  
+//				- sets the corresponding member variable.  
+//  REQ7  - cop3014 must have a VOID FUNCTION to 
+//				- convert numeric average to a final letter grade
+//				- sets the corresponding member variable .  
+//  REQ8  - cop3014 must have get/set functions for all variables
+//  REQ9  - cop3014 must have a function to display: 
+//				- z-number
+//				- All Quiz/Exam scores and Final Grade to the screen.
+//  REQ10 - Test by generating multiple objects, and printing the student object’s record from COP3014.
 //
 
 #include <iostream>
@@ -24,30 +40,45 @@ using namespace std;
 class cop3014
 {
 	public:
+		// setter functions
+		void set_znum(long int new_znumber);
 		bool set_q1_score(short score);
 		void set_q2_score(short score);
 		void set_me_score(short score);
 		void set_fe_score(short score);
 		void set_fcg_n_score(double score);
 		void set_fcg_l_score(char score);
-		void set_znum(long int new_znumber);
 
-		bool validate_score(short score, short score_type);
-
+		// getter functions
+		long int get_znum();
 		short get_q1_score();
 		short get_q2_score();
 		short get_me_score();
 		short get_fe_score();
 		double get_fcg_n_score();
 		char get_fcg_l_score();
-		long int get_znum();
+
+		// other functions
 		double calc_f_grade_num();
 		char calc_f_grade_lett();
+		bool validate_score(short score, short score_type);
 
 	private:
+		// -- CLASS VARIABLES --
+		
 		// Student info
-		long int znumber;			// Student FAU Z-Number
+		long int znumber = 99999999; // Student FAU Z-Number
 				
+		// Variables to hold grades
+		double gr_fcg_n = 999; 		// Final course grade, numeric/%
+		char gr_fcg_l = 'X';		// Final grade, letter/alpha
+		short gr_q1 = 999;			// Holds score for Quiz 1
+		short gr_q2 = 999;			// Holds score for Quiz 2
+		short gr_me = 999;			// Holds score for Midterm Exam
+		short gr_fe = 999;			// Holds score for Final Exam
+
+		// -- CLASS CONSTANTS --
+		
 		// Letter grade mapping to score
 		const short grade_a = 90;
 		const short grade_b = 80;
@@ -58,8 +89,6 @@ class cop3014
 		// Score Types
 		const short st_quiz = 1;
 		const short st_test = 2;
-		// 10+10 = 20
-		// 100
 
 		// Quiz/Test max scores
 		const short qs_max = 10;	// Quiz Score max value 
@@ -71,14 +100,6 @@ class cop3014
 		const double me_w = .30;	// Midterm Exam weight for final grade
 		const double fe_w = .50;	// Final Exam weight for final grade
 
-		// Variables to hold grades
-		double gr_fcg_n;			// Final course grade, numeric/%
-		char gr_fcg_l;				// Final grade, letter/alpha
-		short gr_q1;				// Holds score for Quiz 1
-		short gr_q2;				// Holds score for Quiz 2
-		short gr_me;				// Holds score for Midterm Exam
-		short gr_fe;				// Holds score for Final Exam
-
 };
 
 
@@ -88,47 +109,69 @@ int main()
 	char lc = 'y';  // Outer loop control
 	while (lc == 'y' || lc == 'Y')
 	{
+				
+		// Variable declarations
 		long int new_znumber;
 		short new_score;
-
 		cop3014 student1;
 
-		cout << "Znumber: ";
+		cout << "--- TEST init values ---" << endl;
+		cout << "Q1: " << student1.get_q1_score() << endl;
+		cout << "Q2: " << student1.get_q2_score() << endl;
+		cout << "ME: " << student1.get_me_score() << endl;
+		cout << "FE: " << student1.get_fe_score() << endl;
+		cout << "FG: " << student1.get_fcg_n_score() << endl;
+		cout << "FG: " << student1.get_fcg_l_score() << endl;
+		cout << "--- TEST init values ---" << endl;
+
+		// Prompt for and store Z-Number
+		cout << "Znumber (exactly 8 digits): ";
 		cin >> new_znumber;
 		student1.set_znum(new_znumber);
-		cout << "The Znumber is: " << student1.get_znum() << endl;
 
-		cout << "Quiz 1 score: ";
+		// Prompt for and store Quiz 1 Score
+		cout << "Quiz 1 score (0-10): ";
 		cin >> new_score;
 		student1.set_q1_score(new_score);
-		cout << "Q1 score: " << student1.get_q1_score() << endl;
 		
-		cout << "Quiz 2 score: ";
+		// Prompt for and store Quiz 2 Score
+		cout << "Quiz 2 score (0-10): ";
 		cin >> new_score;
 		student1.set_q2_score(new_score);
-		cout << "Q2 score: " << student1.get_q2_score() << endl;
 		
-		cout << "Midterm Exam score: ";
+		// Prompt for and store Midterm Exam Score
+		cout << "Midterm Exam score (0-100): ";
 		cin >> new_score;
 		student1.set_me_score(new_score);
-		cout << "Midterm score: " << student1.get_me_score() << endl;
 		
-		cout << "Final Exam score: ";
+		// Prompt for and store Final Exam Score
+		cout << "Final Exam score (0-100): ";
 		cin >> new_score;
 		student1.set_fe_score(new_score);
-		cout << "Final score: " << student1.get_fe_score() << endl;
 
-		cout << "FINAL Course Grade numeric: " << student1.calc_f_grade_num() << endl;
+		// Display Z-Number, quiz/test scores and final grade to screen
+		cout << endl;
+		cout << "---------------------------" << endl;
+		cout << "Z-num         : " << student1.get_znum() << endl;
+		cout << "Q1 score      : " << student1.get_q1_score() << endl;
+		cout << "Q2 score      : " << student1.get_q2_score() << endl;
+		cout << "Midterm test  : " << student1.get_me_score() << endl;
+		cout << "Final test    : " << student1.get_fe_score() << endl;
+
+		// Calculate and display final course grade, numeric percent format
+		cout << "Final Grade   : " << student1.calc_f_grade_num() << "% ";
 		
-		cout << "FINAL Course Grade letter: " << student1.calc_f_grade_lett() << endl;
+		// Calculate and display final course grade, letter grade format
+		cout << "/ " << student1.calc_f_grade_lett() << endl;
+		cout << "---------------------------" << endl;
 				
-
 		cout << "Continue? (y/n) ";
 		cin >> lc;
 	}
 	
 	return 0;
 }
+
 
 
 long int cop3014::get_znum()
@@ -215,11 +258,10 @@ char cop3014::get_fcg_l_score()
 };
 
 
-
 double cop3014::calc_f_grade_num()
 {
 	double grade=0;
-	grade = gr_q1 + gr_q2 + gr_me*me_w + gr_fe*fe_w;
+	grade = (double) gr_q1 + (double) gr_q2 + (double) gr_me * (double) me_w + (double) gr_fe * (double) fe_w;
 	set_fcg_n_score(grade);
 
 	return grade;
@@ -229,44 +271,42 @@ double cop3014::calc_f_grade_num()
 char cop3014::calc_f_grade_lett()
 {
 	
-	if (gr_fcg_n >= 90) {
-
-		cout << "got an A" << endl;
+	if (gr_fcg_n >= grade_a) 
+		{
 		set_fcg_l_score('A');
 		return 'A';
-	}
+		}
 
-	if (gr_fcg_n >= 80) {
-
-		cout << "got a B" << endl;
+	if (gr_fcg_n >= grade_b) 
+		{
 		set_fcg_l_score('B');
 		return 'B';
-	}
+		}
 
-	if(gr_fcg_n>=70){
-		cout << "got a C" << endl;
+	if (gr_fcg_n >= grade_c)
+		{
 		set_fcg_l_score('C');
 		return 'C';
-	}
+		}
 
-	if(gr_fcg_n>=60){
+	if(gr_fcg_n >= grade_d)
+		{
 		set_fcg_l_score('D');
-		cout << "got a D" << endl;
 		return 'D';
-	}
+		}
 
-	if (gr_fcg_n <= 59) {
+	if (gr_fcg_n <= grade_f) 
+		{
 		set_fcg_l_score('F');
-		cout << "got an F" << endl;
 		return 'F';
-	}
+		}
+
+	return 'X'; // Indicate invalid grade
 
 }
 
 bool cop3014::validate_score(short score, short score_type)
 {
-	bool output=0;
-
 	if (score < 0)
 		return 0; // invalid score. score must be > 0. 
 
@@ -280,5 +320,5 @@ bool cop3014::validate_score(short score, short score_type)
 		if (score <= es_max)
 			return 1;			// valid
 	}
-
+	return 0; // indicate invalid
 };
