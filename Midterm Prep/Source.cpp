ea36247662b40449ba22e25cd9594ebb2842af68
git-rafa <ralph.benitez@gmail.com>
2022-10-13 19:08:38 -0400

    Added Midterm Prep project

---
+++ Midterm Prep/Source.cpp
@@ -0,0 +1,90 @@
+// COP3014 Midterm Exam Prep 
+// 
+// 
+// 
+// 
+// 
+
+
+#include <cstdlib>      // Standard library
+#include <iostream>     // Needed for i/o streams
+#include <fstream>      // Needed to include files
+#include <ctime>        // Needed to seed rand()
+#include <iomanip>      // Needed for formatting
+
+void lecture1();
+void lecture2();
+void lecture3();
+void lecture4();
+
+
+int main()
+{
+
+	return 0;
+}
+
+void lecture1()
+{
+	// Q1: Declare and initialize two integers variables to zero. The variables are named feet and inches.
+
+	// Q2: Declare and initialize two variables, one int and one double. Both should be initialized to 5.
+
+	// Q3: Write the output statement to prompt for the value to store in 'the_number'
+	
+	// Q4: Write an input statement to place avalue in the variable 'the_number'
+	
+	// Q5: Write an output statement that produces a ‘newline’
+	
+	// Q6: Format output of rational numbers to show 4 decimal places
+
+
+};
+
+void lecture3()
+{
+	//Q1: Assume count = 0 and limit = 10.  Determine the value of these Boolean expressions
+	//	(count == 0) && (limit < 20)
+	
+	//	!(count == 12)
+	
+	
+	//	(limit < 0) && ((limit / count) > 7)
+
+	// Q2: Write an if-else statement that outputs the word "Warning", and, sets the flag to 1, 
+	// provided that either the value of the variable temperature is greater than or equal to 100,
+	// or the value of the variable pressure is greater than or equal to 200, or both. 
+	// Otherwise, the if_else statement outputs the word "OK" and sets the flag to zero. 
+	// The variables are of type int.
+
+
+	// Q3: Write a program for a state that computes tax according to the rate schedule:
+	//   -No tax on first $15,000 of income
+	//   -5% tax on each dollar from $15,001 to $25,000
+	//   -10% tax on each dollar over $25,000
+
+};
+
+void lecture4()
+{
+	//Q1: Assume count = 0 and limit = 10.  Determine the value of these Boolean expressions
+	//	(count == 0) && (limit < 20)
+
+	//	!(count == 12)
+
+
+	//	(limit < 0) && ((limit / count) > 7)
+
+	// Q2: Write an if-else statement that outputs the word "Warning", and, sets the flag to 1, 
+	// provided that either the value of the variable temperature is greater than or equal to 100,
+	// or the value of the variable pressure is greater than or equal to 200, or both. 
+	// Otherwise, the if_else statement outputs the word "OK" and sets the flag to zero. 
+	// The variables are of type int.
+
+
+	// Q3: Write a program for a state that computes tax according to the rate schedule:
+	//   -No tax on first $15,000 of income
+	//   -5% tax on each dollar from $15,001 to $25,000
+	//   -10% tax on each dollar over $25,000
+
+};
